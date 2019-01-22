8892e323db900b53bf7457c7c3d1947fa3773b5a
Weixi Zeng <wzeng@risd.edu>
2019-01-21 22:56:11 -0500

    add footer and change colors

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ document.addEventListener("mousemove", getMouse);
 
 var btnpos = {x:0, y:0};
 
-setInterval(followMouse, 50);
+setInterval(followMouse, 40);
 
 var mouse = {x:0, y:0}; //mouse.x, mouse.y
 
@@ -17,8 +17,8 @@ function followMouse(){
  var distY = mouse.y - btnpos.y;
  //Easing motion
   //Progressive reduction of distance
- btnpos.x += distX/40;
- btnpos.y += distY/20;
+ btnpos.x += distX/100;
+ btnpos.y += distY/50;
 
  btn.style.left = btnpos.x + "px";
  btn.style.top = btnpos.y + "px";
@@ -35,3 +35,8 @@ function getNumber() {
   var number3 = (number1 + "," + number2);
   document.getElementById("displayNumber").innerHTML = number3;
 }
+
+//set interval
+//function for subtract follower
+//function pick a random number to subtract from overall follower numbers
+//
